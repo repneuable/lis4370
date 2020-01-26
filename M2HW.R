ceba8a79bc4c835555791d2d961a3124c5ccbbb0
Kevin <32113593+repneuable@users.noreply.github.com>
2020-01-26 17:34:01 -0500

    Update M2HW.R
    Manually update

--- M2HW.R
+++ M2HW.R
@@ -1,3 +1,5 @@
 #Provided from Module # 2 Assignment instructions
 assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
 myMean <- function(assignment2) { return(sum(assignment)/length(someData)) }
+myMean
+function(assignment2) { return(sum(assignment)/length(someData)) }
