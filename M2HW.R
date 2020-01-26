651a0f73df14ed6c84f6e485bbfed4b8bcf103c6
Kevin <32113593+repneuable@users.noreply.github.com>
2020-01-26 17:51:16 -0500

    Update M2HW.R
    Add wording

--- M2HW.R
+++ M2HW.R
@@ -1,7 +1,17 @@
 #Provided from Module # 2 Assignment instructions
 assignment2 <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
 myMean <- function(assignment2) { return(sum(assignment)/length(someData)) }
-
-#Output
 myMean
 function(assignment2) { return(sum(assignment)/length(someData)) }
+
+#Assign missing variables
+assignment <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+someData <- c(16, 18, 14, 22, 27, 17, 19, 17, 17, 22, 20, 22)
+
+#Function statement without arguments
+myMean
+function(assignment2) { return(sum(assignment)/length(someData)) }
+
+#Function statement with argument
+myMean(assignment2)
+[1] 19.25
