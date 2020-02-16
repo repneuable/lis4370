04f591c462ed111de66a3431d928519c62b5d3b1
Kevin <32113593+repneuable@users.noreply.github.com>
2020-02-16 15:08:23 -0500

    Remove commenting

--- M5HW.R
+++ M5HW.R
@@ -9,9 +9,6 @@
 
 # Find the value of inverse of a matrix, determinant of a matrix by using the following values:
 # A=matrix(1:100, nrow=10) B=matrix(1:1000, nrow=10)
-# Post your result and procedure you took on your blog.
-# A good start will be:
-#   >A <- matrix(1:100, nrow=10)  >B <- matrix(1:1000, nrow=10)
 
 #1. Define martices
 A <- matrix(1:100, nrow=10) 
